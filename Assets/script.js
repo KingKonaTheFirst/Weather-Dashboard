d7f263bd3351e2293c24f2468fc4ceb160b1dfc5
Kona <landon.deroin@gmail.com>
2023-07-13 17:06:45 -0400

    added var to js file

---
+++ Assets/script.js
@@ -0,0 +1,11 @@
+const apiKey = "1fde3780da74ea092aa736114ad5c0d3";
+var city = "";
+var input = document.getElementById("input");
+
+//current day weahter
+var currentTempEl = document.getElementById('current-temp')
+var currentWindEl = document.getElementById('current-wind')
+var currentHumEl = document.getElementById('current-humidity')
+var currentUviEl = document.getElementById('current-uvi')
+var currentCityEl = document.getElementById('city-name')
+var currentDateEl = document.getElementById('current-date')
