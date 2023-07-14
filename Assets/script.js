979874428fd59d9b561a3cf471c4b7b827fc5d32
Kona <landon.deroin@gmail.com>
2023-07-13 20:47:58 -0400

    added more varibles to java and function

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,27 @@
 const apiKey = "1fde3780da74ea092aa736114ad5c0d3";
+
 var city = "";
+
 var input = document.getElementById("input");
 
 //current day weahter
-var currentTempEl = document.getElementById('current-temp')
-var currentWindEl = document.getElementById('current-wind')
-var currentHumEl = document.getElementById('current-humidity')
-var currentUviEl = document.getElementById('current-uvi')
-var currentCityEl = document.getElementById('city-name')
-var currentDateEl = document.getElementById('current-date')
+var currentTempEl = document.getElementById("current-temp");
+
+var currentUviEl = document.getElementById("current-uvi");
+
+var currentWindEl = document.getElementById("current-wind");
+
+var currentHumEl = document.getElementById("current-humidity");
+
+var currentDateEl = document.getElementById("current-date");
+
+var currentCityEl = document.getElementById("city-name");
+
+var entry = document.createElement("li");
+
+var previousSearch = {};
+
+// past search displays
+function clearSearch(){
+    var previousSearch
+}
